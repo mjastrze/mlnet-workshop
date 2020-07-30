b3c9c88976c2f300582a9514bda623609aff9067
SKY.NET Maciej Jastrzebski <maciej.jastrzebski@live.com>
2020-07-30 06:02:41 +0200

    test added

--- test/DataTests/DataValidationTests.cs
+++ test/DataTests/DataValidationTests.cs
@@ -11,36 +11,49 @@ namespace DataTests
     [TestClass]
     public class DataValidationTests
     {
-        private static string TRAIN_DATA_FILEPATH = "";
+        private static string TRAIN_DATA_FILEPATH = @"/media/data/true_car_listings.csv";
+
+        private static IEnumerable<ModelInput> Rows;
 
         [ClassInitialize]
         public static void Initialize(TestContext testContext)
         {
+            var mlContext = new MLContext();
+            var data = mlContext.Data.LoadFromTextFile<ModelInput>(TRAIN_DATA_FILEPATH, hasHeader: true, separatorChar: ',');
 
-        }
-
-        [TestMethod]
-        public void VerifyValidMilage()
-        {
-
+            Rows = mlContext.Data.CreateEnumerable<ModelInput>(data, false);
         }
 
         [TestMethod]
         public void VerifyValidPrice()
         {
+            var hasNegativePrice = Rows.Any(x => x.Price < 0);
 
+            hasNegativePrice.Should().BeFalse();
         }
 
         [TestMethod]
         public void VerifyValidYear()
         {
+            var hasValidYears = Rows.All(x => x.Year > 1950 && x.Year < DateTime.Now.Year + 1);
 
+            hasValidYears.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void VerifyValidMilage()
+        {
+            var hasInvalidMilage = Rows.Any(x => x.Mileage < 0);
+
+            hasInvalidMilage.Should().BeFalse();
         }
 
         [TestMethod]
         public void VerifyMinimumNumberOfRows()
         {
+            var rowCount = Rows.Count();
 
+            rowCount.Should().BeGreaterThan(10000);
         }
     }
 }
